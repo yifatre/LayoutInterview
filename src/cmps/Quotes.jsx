bd62ff2f0c27063ac010f2bfde04e439e0dd68c9
yifatre <yifat.reg@gmail.com>
2024-05-16 16:00:12 +0300

    Update Quotes component styling and structure

--- src/cmps/Quotes.jsx
+++ src/cmps/Quotes.jsx
@@ -6,29 +6,29 @@ import avatar2 from '../assets/img/avatar2.png'
 import avatar3 from '../assets/img/avatar3.png'
 
 export function Quotes() {
-    return <div>
-        <img src={shapes4} alt="" />
-        <h2>Delivering real results for top companies. Some of our success stories.</h2>
-        <ul>
+    return <section className='quotes'>
+        <img className='shapes4' src={shapes4} alt="" />
+        <h2>Delivering real results for top companies. Some of our <span>success stories.</span></h2>
+        <ul className='clean-list flex justify-space-around'>
             <li>
-                <img src={quotes} alt="" />
+                <img className='quts' src={quotes} alt="" />
                 <p>“The team perfectly fit the specialized skill set required. They focused on the most essential features helping us launch the platform eight months faster than planned.”</p>
-                <h4>Kady Baker</h4>
-                <img src={avatar1} alt="" />
+                <h3>Kady Baker</h3>
+                <img className='avatar' src={avatar1} alt="" />
             </li>
             <li>
-                <img src={quotes} alt="" />
+                <img className='quts' src={quotes} alt="" />
                 <p>“We needed to automate our entire onboarding process. The team came in and built out the whole journey. Since going live, user retention has gone through the roof!”</p>
-                <h4>Aiysha Reese</h4>
-                <img src={avatar2} alt="" />
+                <h3>Aiysha Reese</h3>
+                <img className='avatar' src={avatar2} alt="" />
             </li>
             <li>
-                <img src={quotes} alt="" />
+                <img className='quts' src={quotes} alt="" />
                 <p>“Amazing. Our team helped us build an app that delivered a new experience for hiring a physio. The launch was an instant success with 100k downloads in the first month.”</p>
-                <h4>Arthur Clarke</h4>
-                <img src={avatar3} alt="" />
+                <h3>Arthur Clarke</h3>
+                <img className='avatar' src={avatar3} alt="" />
             </li>
         </ul>
-        <img src={shapes5} alt="" />
-    </div>
+        <img className='shapes5' src={shapes5} alt="" />
+    </section>
 }
